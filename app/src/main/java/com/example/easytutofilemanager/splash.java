d0908fe6a8686c5ab254c5d3805250b3a49962de
Manas Mishra <soulstealer806@gmail.com>
2022-06-29 23:45:48 +0530

    File Explorer

---
+++ app/src/main/java/com/example/easytutofilemanager/splash.java
@@ -0,0 +1,26 @@
+package com.example.easytutofilemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        },4000);
+    }
+}
